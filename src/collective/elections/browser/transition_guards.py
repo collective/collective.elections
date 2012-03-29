571eaeaddbdaa6279a5449a7a0e6151557e1494a
Franco Pellegrini <frapell@gmail.com>
2012-03-29 00:44:55 -0300

    More adgustments in the permissions for some states. Added missing guards for some transitions. Added code to check which state we arrived so we can execute needed actions.

--- src/collective/elections/browser/transition_guards.py
+++ src/collective/elections/browser/transition_guards.py
@@ -12,7 +12,7 @@ from datetime import datetime
 class CanSubmitToPublic(grok.View):
     grok.context(IElection)
     grok.name("can-submit-to-public")
-    grok.require("cmf.ModifyPortalContent")
+    grok.require("cmf.ReviewPortalContent")
 
     def __call__(self):
         pm = getToolByName(self.context, 'portal_membership')
@@ -45,8 +45,95 @@ class CanSelectNominees(grok.View):
         
     def render(self):
         return "can-select-nominees"
+
+
+class CanSendToPublic(grok.View):
+    grok.context(IElection)
+    grok.name("can-send-to-public")
+    grok.require("cmf.ReviewPortalContent")
+
+    def __call__(self):
+        pm = getToolByName(self.context, 'portal_membership')
         
+        auth_member = pm.getAuthenticatedMember()
+        ceo = self.context.chief_electoral_officer  
         
+        proper_user = auth_member.getMemberId() == ceo
         
+        if not proper_user:
+            return False
+            
+        # TODO: Check if the PDF and signature fields are populated. If not, fail
+        return True
         
+    def render(self):
+        return "can-send-to-public"
+                
+
+class CanBeStarted(grok.View):
+    grok.context(IElection)
+    grok.name("can-be-started")
+    grok.require("cmf.ReviewPortalContent")
+
+    def __call__(self):
+        vsd = self.context.voting_start_date
+        now = datetime.now()
         
+        return now > vsd
+        
+    def render(self):
+        return "can-be-started"
+
+
+class ShouldBeEnded(grok.View):
+    grok.context(IElection)
+    grok.name("should-be-ended")
+    grok.require("cmf.ReviewPortalContent")
+
+    def __call__(self):
+        ved = self.context.voting_end_date
+        now = datetime.now()
+        
+        return now > ved
+        
+    def render(self):
+        return "should-be-ended"
+
+
+class ResultsShouldGoPublic(grok.View):
+    grok.context(IElection)
+    grok.name("results-should-be-public")
+    grok.require("cmf.ReviewPortalContent")
+
+    def __call__(self):
+        pd = self.context.publication_date
+        now = datetime.now()
+        
+        return now > pd
+        
+    def render(self):
+        return "results-should-be-public"
+
+
+class CanBeClosed(grok.View):
+    grok.context(IElection)
+    grok.name("can-be-closed")
+    grok.require("cmf.ReviewPortalContent")
+
+    def __call__(self):
+        pm = getToolByName(self.context, 'portal_membership')
+        
+        auth_member = pm.getAuthenticatedMember()
+        ceo = self.context.chief_electoral_officer  
+        
+        proper_user = auth_member.getMemberId() == ceo
+        
+        if not proper_user:
+            return False
+            
+        return True
+        
+    def render(self):
+        return "can-be-closed"
+                
+
