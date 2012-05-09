b230b4bfd5a155c6d87708a8b92470e785af6f80
hvelarde <hector.velarde@gmail.com>
2012-05-09 19:48:25 -0300

    updated package information and documentation

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
 
 version = '1.0'
+long_description = open("README.txt").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='collective.elections',
       version=version,
-      description="A voting system for Plone",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="An implementation of the KOA electronic voting system for Plone.",
+      long_description=long_description,
       classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.1",
+        "Framework :: Plone :: 4.2",
         "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
