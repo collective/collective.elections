7bdfb248a8f9c0cee404b06b31d0d346b45ea8e7
hvelarde <hector.velarde@gmail.com>
2013-02-26 16:17:02 -0300

    update distribution

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 
-import os
 from setuptools import setup, find_packages
+
+import os
 
 version = '1.0'
 long_description = open("README.txt").read() + "\n" + \
-                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
-                   open(os.path.join("docs", "HISTORY.txt")).read()
+    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+    open(os.path.join("docs", "HISTORY.txt")).read()
 
-setup(name='collective.elections',
-      version=version,
-      description="An implementation of the KOA electronic voting system for Plone.",
-      long_description=long_description,
-      classifiers=[
+setup(
+    name='collective.elections',
+    version=version,
+    description="An implementation of the KOA electronic voting system for Plone.",
+    long_description=long_description,
+    classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Environment :: Web Environment",
         "Framework :: Plone",
@@ -26,30 +28,31 @@ setup(name='collective.elections',
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone dexterity koa gnupg openpgp',
-      author='Héctor Velarde',
-      author_email='hector.velarde@gmail.com',
-      url='https://github.com/collective/collective.elections',
-      license='GPLv2',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      namespace_packages=['collective'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-        'setuptools',
-        'python-gnupg',
-        'xhtml2pdf',
+    ],
+    keywords='plone dexterity koa gnupg openpgp',
+    author='Héctor Velarde',
+    author_email='hector.velarde@gmail.com',
+    url='https://github.com/collective/collective.elections',
+    license='GPLv2',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['collective'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'borg.localrole',
+        'Pillow',
         'plone.app.dexterity>=1.2.1',
         'plone.principalsource',
-        'borg.localrole',
-        ],
-      extras_require={
+        'python-gnupg',
+        'setuptools',
+        'xhtml2pdf',
+    ],
+    extras_require={
         'test': ['plone.app.testing'],
-        },
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    },
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
